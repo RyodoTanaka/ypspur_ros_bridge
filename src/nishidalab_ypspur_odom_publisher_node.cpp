e92476dd11a4d0fe099ee32f5814e30841ccdb36
RyodoTanaka <k104073r@mail.kyutech.jp>
2015-03-27 13:47:02 +0900

    fix odom
    nishidalab_ypspur_odom_publisherのエラーを解決した。原因は、Spur_init()
    を呼び忘れていたことであった。
    この原因に気づかず、ヘッダファイルを削除してしまったので、次回のコミット
    でこれを直す。

--- src/nishidalab_ypspur_odom_publisher_node.cpp
+++ src/nishidalab_ypspur_odom_publisher_node.cpp
@@ -3,15 +3,34 @@
 #include <nav_msgs/Odometry.h>	        //for odometry
 #include <ypspur.h>		        // for yp-spur
 #include <cmath>	 	        // for math PI
-#include "nishidalab_ypspur_driver/nishidalab_ypspur_odom_publisher_node.hpp"  // include local header
 
 int main(int argc, char** argv){
 
   // init ros
   ros::init(argc, argv, "nishidalab_ypspur_odom_publisher");
+
+  // Yp-spur init
+  if( Spur_init() < 0)
+    ROS_ERROR("can't open spur");
+
+  // set node handler
+  ros::NodeHandle n;
+
+  // set odometory publisher
+  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50); 
+
+  // set tf broad caster
+  tf::TransformBroadcaster odom_broadcaster;
+
+  // odometry pose infomation
+  double x, y, th;
+
+  // velocity twist information
+  double vx, vy, vth;
+
+  // ros timer
+  ros::Time current_time = ros::Time::now();
   
-  // original class
-  NishidalabYpspurOdomPublisher NlabYspurOdomPub;
   // for quaternion
   geometry_msgs::Quaternion odom_quat;
   // for transform tf
@@ -22,66 +41,58 @@ int main(int argc, char** argv){
   // loop rate is 25.0 [Hz]
   ros::Rate r(25.0);
 
-  double x, y, th;
-  double vx, vth;
 
   ///////////////////////////////////////////////////
   // LOOP START
   ///////////////////////////////////////////////////
-  while(NlabYspurOdomPub.n.ok()){
+  while(n.ok()){
     // get current time
-    NlabYspurOdomPub.current_time = ros::Time::now();
+    current_time = ros::Time::now();
     
     // Get odometory pose information
-    Spur_get_pos_GL(&x, &y, &th);//&NlabYspurOdomPub.x, &NlabYspurOdomPub.y, &NlabYspurOdomPub.th );
-    NlabYspurOdomPub.x = x;
-    NlabYspurOdomPub.y = y;
-    NlabYspurOdomPub.th = th;
+    Spur_get_pos_GL(&x, &y, &th);
 
     // get current velocity
-    Spur_get_vel(&vx, &vth);//&NlabYspurOdomPub.vx, &NlabYspurOdomPub.vth );
-    NlabYspurOdomPub.vx = vx;
-    NlabYspurOdomPub.vth = vth;
-    NlabYspurOdomPub.vy = 0;
-
+    Spur_get_vel(&vx, &vth);
+    vy = 0;
     // TF transform section -----------------------------------------------------------
     //since all odometry is 6DOF we'll need a quaternion created from yaw
-    odom_quat = tf::createQuaternionMsgFromYaw(NlabYspurOdomPub.th);
+    odom_quat = tf::createQuaternionMsgFromYaw(th);
 
     //first, we'll publish the transform over tf
-    odom_trans.header.stamp = NlabYspurOdomPub.current_time;
+    odom_trans.header.stamp = current_time;
     odom_trans.header.frame_id = "odom";
     odom_trans.child_frame_id = "base_link";
 
-    odom_trans.transform.translation.x = NlabYspurOdomPub.x;
-    odom_trans.transform.translation.y = NlabYspurOdomPub.y;
+    odom_trans.transform.translation.x = x;
+    odom_trans.transform.translation.y = y;
     odom_trans.transform.translation.z = 0.0;
     odom_trans.transform.rotation = odom_quat;
 
     //send the transform
-    NlabYspurOdomPub.odom_broadcaster.sendTransform(odom_trans);
+    odom_broadcaster.sendTransform(odom_trans);
     // TF transform section end -------------------------------------------------------
     
 
     // Odom section -------------------------------------------------------------------
     //next, we'll publish the odometry message over ROS
-    odom.header.stamp = NlabYspurOdomPub.current_time;
+    odom.header.stamp = current_time;
     odom.header.frame_id = "odom";
 
     //set the position
-    odom.pose.pose.position.x = NlabYspurOdomPub.x;
-    odom.pose.pose.position.y = NlabYspurOdomPub.y;
+    odom.pose.pose.position.x = x;
+    odom.pose.pose.position.y = y;
     odom.pose.pose.position.z = 0.0;
     odom.pose.pose.orientation = odom_quat;
 
     //set the velocity
     odom.child_frame_id = "base_link";
-    odom.twist.twist.linear.x = NlabYspurOdomPub.vx;
-    odom.twist.twist.linear.y = NlabYspurOdomPub.vy;
-    odom.twist.twist.angular.z = NlabYspurOdomPub.vth;
+    odom.twist.twist.linear.x = vx;
+    odom.twist.twist.linear.y = vy;
+    odom.twist.twist.angular.z = vth;
 
     //publish the message
-    NlabYspurOdomPub.odom_pub.publish(odom);
+    odom_pub.publish(odom);
     // Odom section end ----------------------------------------------------------------
 
     r.sleep();
