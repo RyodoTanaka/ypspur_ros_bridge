74aa77ad52ffd6108883ecbe842bd016c88bd310
Ryodo <k104073r@mail.kyutech.jp>
2017-11-02 06:30:47 +0900

    [modify] change file name & add parameter getter

--- include/ypspur_ros_bridge/ypspur_ros_bridge_driver_node.hpp
+++ include/ypspur_ros_bridge/ypspur_ros_bridge_driver_node.hpp
@@ -3,11 +3,11 @@
 #include <ypspur.h>		        // for yp-spur
 #include <cmath>	 	        // for math PI
 
-class NishidalabYpspurDriver
+class YpspurROSBridgeDriver
 {
 public:
-  NishidalabYpspurDriver();
-  ~NishidalabYpspurDriver();
+  YpspurROSBridgeDriver();
+  ~YpspurROSBridgeDriver();
   
 private:
   // Call back
@@ -25,21 +25,23 @@ private:
 
 };
 
-NishidalabYpspurDriver::NishidalabYpspurDriver() : 
+YpspurROSBridgeDriver::YpspurROSBridgeDriver() : 
   linear_vel_max(1.1),
   angular_vel_max(M_PI),
   linear_acc_max(1.0),
   angular_acc_max(M_PI)
 {
+  ros::NodeHandle nh("~");
+  
   // init yp-spur---------------------------------------------------------------------
   if( Spur_init() < 0)
     ROS_ERROR("can't open ypspur");
 
   // using parameter server
-  n.param("nishidalab_ypspur/linear_vel_max", linear_vel_max, linear_vel_max);
-  n.param("nishidalab_ypspur/angular_vel_max", angular_vel_max, angular_vel_max);
-  n.param("nishidalab_ypspur/linear_acc_max", linear_acc_max, linear_acc_max);
-  n.param("nishidalab_ypspur/angular_acc_max", angular_acc_max, angular_acc_max);
+  nh.param("linear_vel_max", linear_vel_max, linear_vel_max);
+  nh.param("angular_vel_max", angular_vel_max, angular_vel_max);
+  nh.param("linear_acc_max", linear_acc_max, linear_acc_max);
+  nh.param("angular_acc_max", angular_acc_max, angular_acc_max);
   // init velocity & accelaration limits (Unit is m/s & rad/s)
   Spur_set_vel(linear_vel_max);
   Spur_set_accel(linear_acc_max);
@@ -47,11 +49,11 @@ NishidalabYpspurDriver::NishidalabYpspurDriver() :
   Spur_set_angaccel(angular_acc_max);
   // end init yp-spur------------------------------------------------------------------
 
-  cmd_vel_sub = n.subscribe<geometry_msgs::Twist>("cmd_vel", 10, &NishidalabYpspurDriver::Callback, this);
+  cmd_vel_sub = n.subscribe<geometry_msgs::Twist>("cmd_vel", 10, &YpspurROSBridgeDriver::Callback, this);
 
 }
 
-NishidalabYpspurDriver::~NishidalabYpspurDriver()
+YpspurROSBridgeDriver::~YpspurROSBridgeDriver()
 {
   Spur_stop();
   Spur_free();
@@ -59,6 +61,6 @@ NishidalabYpspurDriver::~NishidalabYpspurDriver()
 
 }
 
-void NishidalabYpspurDriver::Callback(const geometry_msgs::Twist::ConstPtr& cmd_vel){
+void YpspurROSBridgeDriver::Callback(const geometry_msgs::Twist::ConstPtr& cmd_vel){
   Spur_vel(cmd_vel->linear.x, cmd_vel->angular.z);
 }
